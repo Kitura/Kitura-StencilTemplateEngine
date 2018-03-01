b80eda8a1b89308750af779882f3b749bb84f29e
David Jones <djones6dev@gmail.com>
2018-03-01 18:42:58 +0000

    Restore Swift 3.1 support (#27)
    - but disable Swift 4.1 testing until PathKit supports 4.1
    - Add missing .git to Stencil in Swift 3 package (as per #23)

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 /**
  * Copyright IBM Corporation 2016, 2018
  *
@@ -21,24 +18,8 @@ import PackageDescription
 
 let package = Package(
     name: "KituraStencil",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "KituraStencil",
-            targets: ["KituraStencil"]
-        )
-    ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "2.0.0"),
-        .package(url: "https://github.com/kylef/Stencil.git", .upToNextMinor(from: "0.10.0"))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "KituraStencil",
-            dependencies: ["KituraTemplateEngine", "Stencil"]
-        )
+        .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 10)
     ]
 )
-
