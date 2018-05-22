1b66e77c7d63524c6b7717bf8cb03c289b3e4ddc
David Dunn <ddunnibm@gmail.com>
2018-05-22 14:39:55 +0100

    Add type safe render method  (#33)

--- Sources/KituraStencil/KituraStencilTemplateEngine.swift
+++ Sources/KituraStencil/KituraStencilTemplateEngine.swift
@@ -18,10 +18,27 @@ import Foundation
 import KituraTemplateEngine
 import Stencil
 import PathKit
+import Foundation
 
+// StencilTemplateEngineError for Error handling.
 public enum StencilTemplateEngineError: Swift.Error {
+    // Thrown when StencilTemplateEngine.rootPaths property is empty.
     case rootPathsEmpty
-    case deprecatedRenderMethodCalled // call render(filePath, context, options, templateName)
+    
+    // Call render(filePath, context, options, templateName).
+    case deprecatedRenderMethodCalled
+    
+    // Thrown when unable to cast 'json' value to a [String: Any].
+    case unableToCastJSONToDict
+    
+    // Thrown when unable to encode the Encodable value provided to data.
+    case unableToEncodeValue(value: Encodable)
+    
+    // Thrown when Stencil fails to render the context with the given template.
+    case unableToRenderContext(context: [String: Any])
+    
+    //Thrown when an array or set of Encodables is passed without a Key.
+    case noKeyProvidedForType(value: Encodable)
 }
 
 public class StencilTemplateEngine: TemplateEngine {
@@ -40,7 +57,7 @@ public class StencilTemplateEngine: TemplateEngine {
     public func render(filePath: String, context: [String: Any]) throws -> String {
         throw StencilTemplateEngineError.deprecatedRenderMethodCalled
     }
-
+    
     public func render(filePath: String, context: [String: Any], options: RenderingOptions,
                        templateName: String) throws -> String {
         if rootPaths.isEmpty {
@@ -51,6 +68,46 @@ public class StencilTemplateEngine: TemplateEngine {
         let environment = Environment(loader: loader, extensions: [`extension`])
         var context = context
         context["loader"] = loader
-        return try environment.renderTemplate(name: templateName,  context: context)
+        do {
+            return try environment.renderTemplate(name: templateName,  context: context)
+        } catch {
+            throw StencilTemplateEngineError.unableToRenderContext(context: context)
+        }
+    }
+
+    public func render<T: Encodable>(filePath: String, with value: T, forKey key: String?,
+                                   options: RenderingOptions, templateName: String) throws -> String {
+        if rootPaths.isEmpty {
+            throw StencilTemplateEngineError.rootPathsEmpty
+        }
+        
+        //Throw an error if an array is passed without providing a key.
+        if key == nil {
+            let mirror = Mirror(reflecting: value)
+            if mirror.displayStyle == .collection || mirror.displayStyle == .set {
+                throw StencilTemplateEngineError.noKeyProvidedForType(value: value)
+            }
+        }
+        
+        let json: [String: Any]
+        
+        if let contextKey = key {
+            json = [contextKey: value]
+        } else {
+            var data = Data()
+            do {
+                data = try JSONEncoder().encode(value)
+            } catch {
+                throw StencilTemplateEngineError.unableToEncodeValue(value: value)
+            }
+            
+            guard let dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+                throw StencilTemplateEngineError.unableToCastJSONToDict
+            }
+            
+            json = dict
+        }
+        
+        return try render(filePath: filePath, context: json, options: options, templateName: templateName)
     }
 }
